d9d739a410e495460e920f1286d7e1d0fdb9b638
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-05 19:40:31 +0500

    sattisauda client

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://sattisauda-cargo.kz/:3001',
+    
+    nameCargo: 'Satti Sauda Cargo'
+  };
+  
+  
+export default config;
+  
